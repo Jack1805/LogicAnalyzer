5331ddd81b813ab426d25af000fd5aac9839c871
Jack1805 <60979553+Jack1805@users.noreply.github.com>
2026-08-16 15:41:34 +0700

    test: add intentional out-of-bounds write for inline AI review

--- 03_Code/src/main.c
+++ 03_Code/src/main.c
@@ -4,8 +4,12 @@ int main(void)
 {
     uint16_t adc_raw = 1023u;
     uint8_t adc_value = adc_raw;
+    uint8_t samples[4] = {0u};
+
+    samples[4] = adc_value;
 
     (void)adc_value;
+    (void)samples;
 
     while (1)
     {
